fd81130b98491575ee5f97d44240347d9896d001
kotelnikovalizo4ka <kotelnikovalizo4ka>
2025-04-29 22:57:32 +0300

    message

--- seminars/seminar_11_04_2025/try_requests.py
+++ seminars/seminar_11_04_2025/try_requests.py
@@ -12,7 +12,7 @@ except ImportError:
 
 if __name__ == "__main__":
     # Step 1. GET request
-    url = "https://www.nn.ru/"
+    url = "https://klops.ru/"
     response = requests.get(url)
     print(f"Status code: {response.status_code}")
     print(f"First 500 chars:\n{response.text[:500]}...")
